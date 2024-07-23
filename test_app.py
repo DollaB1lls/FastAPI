bceb0d0b4452103646708f7740582473f6370b52
Devin Dolla <dmdolla02@gmail.com>
2024-07-23 11:15:24 -0400

    minor changes

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 from fastapi.testclient import TestClient
 from pymongo import MongoClient
 from bson import ObjectId
-import pytest
+#import pytest
 from main import app
 
 client = TestClient(app)
 mongo_client = MongoClient('mongodb://localhost:27017/')
 db = mongo_client['courses']
+
+print(db.data)
